2ad3ec625fdac4fccde1f5f1d2ead1751675855f
KHWAJAMOIN ABALE <167965225+moinabale@users.noreply.github.com>
2024-05-14 00:21:22 +0530

    Add files via upload

---
+++ loopdemo.java
@@ -0,0 +1,11 @@
+public class loopdemo {
+    public static void main(String args[]){
+        int sum=0;
+        for(int i=0;i<=10;i++){
+            sum=sum+i;
+        }
+    System.out.println("sum of first 10 number is:"+sum);
+    }
+
+    
+}
